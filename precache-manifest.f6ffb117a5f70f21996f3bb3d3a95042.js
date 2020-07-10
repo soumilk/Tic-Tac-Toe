0fbf0a664986353e6c1742cc3133b93262491aed
soumilk <soumilk.k@gmail.com>
2020-07-10 10:01:53 +0530

    Updates

---
+++ precache-manifest.f6ffb117a5f70f21996f3bb3d3a95042.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "756fbf9dd9413c58bcec8f904e4620ea",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "e8b1b037b93029110058",
+    "url": "/Tic-Tac-Toe/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b2f83c21d5d19d8adb1f",
+    "url": "/Tic-Tac-Toe/static/css/main.2d760aae.chunk.css"
+  },
+  {
+    "revision": "e8b1b037b93029110058",
+    "url": "/Tic-Tac-Toe/static/js/2.5d50787a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Tic-Tac-Toe/static/js/2.5d50787a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f83c21d5d19d8adb1f",
+    "url": "/Tic-Tac-Toe/static/js/main.3a26dc45.chunk.js"
+  },
+  {
+    "revision": "5965f36d7f72120b2bad",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.49965c0b.js"
+  }
+]);
